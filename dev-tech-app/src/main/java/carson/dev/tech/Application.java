af7c8d6fa3b768650e0a266bfca9ac5f0dac52a7
Guanhong Liu <guanhongliu45@gmail.com>
2025-06-10 11:32:44 -0400

    Remove redisson-spring-boot-starter and rely on manual RedissonConfig

--- dev-tech-app/src/main/java/carson/dev/tech/Application.java
+++ dev-tech-app/src/main/java/carson/dev/tech/Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 📌 exclude this class:
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = RedissonAutoConfiguration.class
-)
+//@SpringBootApplication(
+//    exclude = RedissonAutoConfiguration.class
+//)
+@SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
